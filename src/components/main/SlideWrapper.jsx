32a35c15de30fb2c85d844b56e17d44c767c51b9
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-12-01 01:33:34 +0300

    some fixes

--- src/components/main/SlideWrapper.jsx
+++ src/components/main/SlideWrapper.jsx
@@ -6,9 +6,15 @@ import 'swiper/css/navigation'
 import 'swiper/css/effect-fade'
 import SwiperCore, { Autoplay } from 'swiper';
 
-import slide1 from '../../assets/vo.png'
+import slide1 from '../../assets/cloud.png'
 import slide2 from '../../assets/slide2.png'
-import {Link} from 'react-router-dom'
+import slide3 from '../../assets/ml2.png'
+import slide4 from '../../assets/bd.png'
+import slide5 from '../../assets/ai.png'
+import slide6 from '../../assets/bi.png'
+import slide7 from '../../assets/ro.png'
+import slide8 from '../../assets/bl.png'
+import slide9 from '../../assets/au.png'
 
 export function SlideWrapper () {
     SwiperCore.use([Autoplay])
@@ -27,18 +33,75 @@ export function SlideWrapper () {
         >
         <SwiperSlide className='slide'>
         <div className="flex">
-            <h2>Главная Финтех компания <br /> Вологодский Области</h2>
-            <a href='#footer'><button className='cta'>Узнать больше</button></a>
+            <h2>Самое мощное облако в Вологде <br /> Первый месяц бесплатно </h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
         </div>
         <img src={slide1} alt="изображение на слайдер" className='silde-img slide-img1'/>
-
         </SwiperSlide>
+
         <SwiperSlide className='slide'>
             <div className="flex">
-                <h2>Большой перечень <br /> Разнообразных услуг</h2>
-                <a href="#footer"><button className='cta'>Узнать больше</button></a>
+                <h2>Современные и удобные кассы <br />Скидки для малого бизнеса </h2>
+                <a href="#portal"><button className='cta'>Узнать больше</button></a>
             </div>
             <img src={slide2} alt="изображение на слайдер" className='silde-img slide-img2' />
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+        <div className="flex">
+            <h2>Эффективное машинное обучение <br />
+            Гарантируем большие конверсии продаж</h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
+        </div>
+        <img src={slide3} alt="изображение на слайдер" className='silde-img slide-img3'/>
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+            <div className="flex">
+                <h2>Персональная аналитика. <br />Подскажем выгодное применение <br /> Собранной <span className='str'>Big Data</span></h2>
+                <a href="#portal"><button className='cta'>Узнать больше</button></a>
+            </div>
+            <img src={slide4} alt="изображение на слайдер" className='silde-img slide-img4' />
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+        <div className="flex">
+            <h2>Внедрим искусственный интеллект <br />  Скидка 50% новым пользователям</h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
+        </div>
+        <img src={slide5} alt="изображение на слайдер" className='silde-img slide-img5'/>
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+            <div className="flex ff1">
+                <h2>Масштабируемая биометрия<br />Поможем узнать все о ваших струдниках</h2>
+                <a href="#portal"><button className='cta'>Узнать больше</button></a>
+            </div>
+            <img src={slide6} alt="изображение на слайдер" className='silde-img slide-img6' />
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+        <div className="flex">
+            <h2>Роботизируем мануальные процессы <br /> Производительность труда возрастет вдвое</h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
+        </div>
+        <img src={slide7} alt="изображение на слайдер" className='silde-img slide-img7'/>
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+        <div className="flex">
+            <h2>Спроектируем вашу собственную <br /> блокчейн платформу.<br />Предложение ограничено</h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
+        </div>
+        <img src={slide8} alt="изображение на слайдер" className='silde-img slide-img8'/>
+        </SwiperSlide>
+
+        <SwiperSlide className='slide'>
+        <div className="flex">
+            <h2>Автоматизируем и оптимизируем <br />Ваши бизнес-процессы.<br />Скидка новым клиентам</h2>
+            <a href='#portal'><button className='cta'>Узнать больше</button></a>
+        </div>
+        <img src={slide9} alt="изображение на слайдер" className='silde-img slide-img9'/>
         </SwiperSlide>
 
         </Swiper>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,12 @@
 import product1 from '../../assets/product1.png'
-import product2 from '../../assets/cloud.png'
+import product2 from '../../assets/cloud2.jpg'
 import product3 from '../../assets/ml.jpg'
-
+import product4 from '../../assets/ai2.jpeg'
+import product5 from '../../assets/ro2.avif'
+import product6 from '../../assets/bi2.jpg'
+import product7 from '../../assets/bl2.jpeg'
+import product8 from '../../assets/bg2.jpeg'
+import product9 from '../../assets/au2.webp'
 
 export function Products() {
 
@@ -9,18 +14,10 @@ export function Products() {
     <div className="content general-content products">
       <div className="container p1">
           <h2 className='heading' id='about'>Каталог</h2>
-          <div className="productBody">
-            <img src={product1} alt="касса" className='product1'/>
-            <div className="description1">
-              <h2>Мобильная касса <br /> с предустановленным ПО</h2>
-              <h3>Все кассы подходят для торговли маркированными товарами и крепким алкоголем.</h3>
-              <h3 className='price'>от 1 950 ₽ в рассрочку</h3>
-            </div>
-          </div>
 
           <div className="productBody">
-            <img src={product2} alt="касса" className='product1'/>
-            <div className="description1 d2">
+            <img src={product2} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
               <h2>Облачная инфраструктура</h2>
               <h3>Готовые вычислительные ресурсы в облаке
                     для построения <br /> отказоустойчивой ИТ-инфраструктуры <br /> для бизнеса и физических лиц.</h3>
@@ -29,13 +26,77 @@ export function Products() {
           </div>
 
           <div className="productBody">
-            <img src={product3} alt="касса" className='product1 i1'/>
+            <img src={product1} alt="товар" className='product1'/>
+            <div className="description1">
+              <h2>Мобильная товар <br /> с предустановленным ПО</h2>
+              <h3>Все кассы подходят для торговли маркированными товарами и крепким алкоголем.</h3>
+              <h3 className='price'>от 1 950 ₽ в рассрочку</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product3} alt="товар" className='product1 i1'/>
             <div className="description1 d3 f1">
               <h2>Машинное обучение</h2>
               <h3>Разработка прогнозных систем оттока, поведения потребителей, 
                 прогноза <br /> поломок и нагрузки оборудования, прогнозирования конверсии продаж, спроса и страхового 
                 случая, <br /> предсказания трифика, ДТП и т.д.</h3>
               <h3 className='price p3 p4'>от 30 000 ₽</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product4} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Искусственный интеллект</h2>
+              <h3>Интеллектуальный анализ данных на основе математического анализа <br /> выявляет закономерности и тенденции, существующие в данных, которые невозможно обнаружить при традиционном анализе.</h3>
+              <h3 className='price p3 p4'>от 30 000 ₽</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product5} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Роботизация</h2>
+              <h3>В соответствии с требованиями клиентов, компания производит роботов, обладающих превосходными характеристиками, а также разрабатывает и производит рабочие приспособления и логистические конвейерные линии, предназначенные для использования роботов в составе автоматических линий.</h3>
+              <h3 className='price p3 p4'>от 30 000 ₽</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product6} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Биометрия</h2>
+              <h3>Разрабатываем биометрических систем учета и слежения под ключ.</h3>
+              <h3 className='price p3'>от 15 000 ₽</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product7} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Блокчейн</h2>
+              <h3>Настроим и запустим платформу за <br /> 3 дня с момента оплаты. Тысячи токенов <br /> и криптовалют поддерживаются по умолчанию. Добавление других — за пару часов
+                 </h3>
+              <h3 className='price'>от 100 000 ₽ в рассрочку</h3>
+            </div>
+          </div>
+          
+          <div className="productBody">
+            <img src={product8} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Big Data</h2>
+              <h3> Строим хранилища данных, развиваем и поддерживаем существующие. Настраиваем интеграционные потоки из различных источников. Обеспечиваем консолидацию и контроль качества данных, мгновенный расчет <br /> аналитических показателей.</h3>
+              <h3 className='price p3 p4'>от 20 000 ₽</h3>
+            </div>
+          </div>
+
+          <div className="productBody">
+            <img src={product9} alt="товар" className='product1 i1'/>
+            <div className="description1 d3 f1">
+              <h2>Автоматизация и <br /> Оптимизация процессов </h2>
+              <h3>Мы проводим полноценный аудит, погружаемся в ваш бизнес, описываем процессы, фиксируем первичные показатели и составляем Техническое Задание и предлагаем вам наивыгодный вариант.</h3>
+              <h3 className='price p3 p4'>от 50 000 ₽</h3>
             </div>
           </div>
 
